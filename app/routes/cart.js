7612f4c64d3fcc2d67b29ff1573a5015ea71ee0b
aksingh1 <ajaykumar.singh1@globallogic.com>
2017-01-17 16:46:46 +0530

    4th commit

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,10 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	
-
+    actions:{
+        removeProduct(order){
+            return this.get('store').removeOrder(order);
+        }
+    },
 	model() {
-		return this.get('store').getProducts();
-	},
-    store:Ember.inject.service()
+		return Ember.RSVP.hash({
+           itemDetail: this.get('store').getOrders(),
+           totalPrice: this.getTotalPrice()
+        })
+	}, 
+    store:Ember.inject.service(),
+
+    getTotalPrice() {
+        const placedOrder = this.get('store').getOrders()
+        let totalPrice=0;
+        placedOrder.forEach((item) =>{
+          totalPrice = totalPrice + item.price;
+        });
+        return totalPrice;
+    }
 });
--- app/routes/description.js
+++ app/routes/description.js
@@ -2,16 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-
 	model(params){
       return this.get('store').getProducts().findBy('pid',params.pid);
     },
 
     actions:{
-    	addToCart(pid){
-    		window.alert("Your Product has been added successfully.");
-    		this.get('cart').pushObject('model.pid');
-    		console.log(this.get(cart));
+    	addToCart(model){
+    		window.alert("Your Product has been added successfully."+model.pid);
+    		this.get('store').saveOrder(model);
+            console.log(this.get('store').getOrders());
     	}
     } 
     
